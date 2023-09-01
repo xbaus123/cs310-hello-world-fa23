9d76c25836f3a312eb85682b488072620823cae6
Xavier Bausley <xbausley@stu.jsu.edu>
2023-09-01 12:01:36 -0500

    Changed name of HelloWorld Classes

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,8 +6,8 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        Class c1 = new Class1();
-        Class c2 = new Class2();
+        Class Class1 = new Class1();
+        Class Class2 = new Class2();
         System.out.println("Hello, World!");
     }
 
